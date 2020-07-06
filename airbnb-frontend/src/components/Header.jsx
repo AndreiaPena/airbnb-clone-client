f6f80908bf3ffbcb60083a36e1002dc3ba6a2430
AndreiaPena <andreia.pena.ferreira@gmail.com>
2020-07-06 15:15:06 +0200

    création du projet, ajout des packages

---
+++ airbnb-frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <nav id="navigation">
+      <h1 href="#" className="logo">
+        Airbnb
+      </h1>
+    </nav>
+  );
+};
+export default Header;
